a676d87509b7a9d473081377a283e38191811f44
Jihogrammer <jiho@kakao.com>
2022-06-09 15:41:21 +0900

    📝 takeaway

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 // app.js (shared between server and client)
 
-import { createSSRApp } from 'vue'
+import { createSSRApp, ref, onMounted, useSSRContext } from 'vue'
 
 export function createApp() {
 	return createSSRApp({
 		data: () => ({ count: 1 }),
 		template: `<button @click="count++">{{ count }}</button>`
 	})
+}
+
+export function createHooksApp() {
+	return createSSRApp({
+		setup() {
+			const context = ref(null);
+
+			if (typeof window === 'undefined') {
+				context.value = useSSRContext();
+				console.log({ context: context.value });
+			}
+
+			const count = ref(0);
+			function increment() { count.value++; }
+			const decrement = () => { count.value--; };
+
+			onMounted(() => {
+				increment();
+			});
+
+			return { count, increment, decrement, context };
+		},
+		template: `
+			<div>
+				<button @click="decrement"> - </button>
+				{{ count }}
+				<button @click="increment"> + </button>
+				<hr>
+				<p>{{ context.message }}</p>
+			</div>`
+	});
 }
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { renderToString } from 'vue/server-renderer'
-import { createApp } from './app.js'
+import { createApp, createHooksApp } from './app.js'
 
 const server = express()
 server.use(express.static('.'))
 
 server.get('/', (req, res) => {
-	const app = createApp()
+	const app = createApp();
 
 	renderToString(app).then((html) => {
 		res.send(`
@@ -27,10 +27,40 @@ server.get('/', (req, res) => {
 				<div id="app">${html}</div>
 			</body>
 		</html>
-		`)
+		`);
+	});
+});
+
+server.get('/hooks', (req, res) => {
+	const app = createHooksApp();
+	const context = { message: 'This message from Server' };
+
+	renderToString(app, context).then((html) => {
+		res.send(`
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Vue SSR Hooks</title>
+				<script type="importmap">
+					{
+						"imports": {
+							"vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+						}
+					}
+				</script>
+				<script>
+					window.context = ${JSON.stringify(context)}
+				</script>
+				<script type="module" src="/hooks-client.js"></script>
+			</head>
+			<body>
+				<div id="app">${html}</div>
+			</body>
+		</html>
+		`);
 	})
-})
+});
 
 server.listen(3000, () => {
-	console.log('ready')
-})
+	console.log('ready');
+});
